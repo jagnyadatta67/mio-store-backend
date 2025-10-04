54034e6767362a0db46c059ddc50d031019f3563
jagnya <jagnya.panigrahi@landmarkgroup.in>
2025-10-04 19:11:02 +0530

    first Commit

---
+++ miostore/src/main/java/com/miostore/miostore/MiostoreApplication.java
@@ -0,0 +1,13 @@
+package com.miostore.miostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiostoreApplication.class, args);
+	}
+
+}
---
+++ miostore/src/test/java/com/miostore/miostore/MiostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miostore.miostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
